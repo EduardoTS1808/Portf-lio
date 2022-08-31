846f1b52790a2de5357c5bd0e8a182d45d67e484
Eduardo Tavares <eduardo91423344@gmail.com>
2022-08-31 20:33:00 -0300

    primeiro commit de hoje 31/agosto

---
+++ mod-11/post-blog/HTML-CSS/prontotb/main.js
@@ -0,0 +1,35 @@
+// let a = new Object();
+// let book = {
+//     "main title" : "javascript",
+//      'sub-title' : 'the definitive guide',
+//      "for" : "all audiences",
+
+//      author : {
+//         firstname: "David",
+//         surname: "flanagan"
+//      }
+// };
+
+ 
+
+
+
+
+
+
+
+
+// document.body.innerText = JSON.stringify(address)
+
+
+
+
+
+
+
+
+
+// sintaxe  do operador for in
+// for (variable in array) {
+//     code
+//  }
