fe9fd2f8529145ececbf35edbd2cae6b0b77dc6b
Eduardo Tavares <eduardo91423344@gmail.com>
2022-09-05 21:57:44 -0300

    pronto para enviar 0.1

--- mod-11/post-blog/HTML-CSS/prontotb/main.js
+++ mod-11/post-blog/HTML-CSS/prontotb/main.js
@@ -1,35 +1,6 @@
-// let a = new Object();
-// let book = {
-//     "main title" : "javascript",
-//      'sub-title' : 'the definitive guide',
-//      "for" : "all audiences",
-
-//      author : {
-//         firstname: "David",
-//         surname: "flanagan"
-//      }
-// };
-
- 
-
-
-
-
-
-
-
-
-// document.body.innerText = JSON.stringify(address)
-
-
-
-
-
-
-
-
-
-// sintaxe  do operador for in
-// for (variable in array) {
-//     code
-//  }
+let btnM = document.querySelector("#btn-M");
+let ulLincks = document.querySelector(".lincks");
+btnM.onclick = () => {
+    btnM.classList.toggle('fa-times');
+    ulLincks.classList.toggle('active');
+}
